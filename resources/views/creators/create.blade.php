a097d506d99ccf16b80b0e57e756bde2e9ea32bc
yanitsa395 <qnicaa395@abv.bg>
2019-01-26 19:18:08 +0200

    First commit

--- resources/views/creators/create.blade.php
+++ resources/views/creators/create.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+
+@section('content')
         <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -87,3 +89,4 @@
 </div>
 </body>
 </html>
+@endsection
